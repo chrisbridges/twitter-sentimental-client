af1025dceffe5a7da9f3d168971d7d3f35714137
chris.bridges <christopher_bridges@icloud.com>
2018-06-08 12:45:14 -0400

    basic router func set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import {connect} from 'react-redux';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import StockSearch from './components/stockSearch';
 import SentimentScore from './components/sentimentScore';
 import SentimentWords from './components/sentimentWords';
@@ -10,16 +11,27 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header>
-          <h1>Pulse</h1>
-          <h2>Performing Real-time Sentimental Analysis on Stocks</h2>
-        </header>
-        <StockSearch />
-        <SentimentScore />
-        <SentimentWords />
-        <Tweets />
-      </div>
+      <Router>
+        <div className="App">
+          <header>
+            <h1>Pulse</h1>
+            <h2>Performing Real-time Sentimental Analysis on Stocks</h2>
+          </header>
+          <StockSearch />
+          <main>
+            <Route exact path="/analysis" component={() => 
+              <div>
+                <Tweets />
+                <SentimentWords />
+                <SentimentScore />
+              </div>} 
+            />
+            {/* <Tweets />
+            <SentimentWords />
+            <SentimentScore /> */}
+          </main>
+        </div>
+      </Router>
     );
   }
 }
