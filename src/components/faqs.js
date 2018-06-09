285aa2a04c4fd5420c05b91d09e346f4bc5064a3
chris.bridges <christopher_bridges@icloud.com>
2018-06-09 14:54:06 -0400

    basic FAQ CSS

--- src/components/faqs.js
+++ src/components/faqs.js
@@ -63,12 +63,16 @@ export class FAQs extends Component {
   	// some service returning a list of FAQs
   	const faqs = [
       {
-        question: 'Question 1',
-        answer: 'answer 1'
+        question: 'What is sentimental analysis?',
+        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt viverra augue, non lacinia mi fermentum nec. Sed sed tellus vel ligula euismod ultrices. Etiam vitae neque pretium, efficitur neque sed, scelerisque est. Sed convallis diam ut leo interdum sollicitudin. Quisque laoreet interdum ante ut mollis. Fusce vel mauris nec erat gravida pretium. Maecenas porttitor libero tellus, ut tempus quam porta ac. Nulla pretium felis ligula, quis dapibus dolor facilisis in.'
       },
       {
-        question: 'Question 2',
-        answer: 'answer 2'
+        question: 'How are you conducting this analysis?',
+        answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt viverra augue, non lacinia mi fermentum nec. Sed sed tellus vel ligula euismod ultrices. Etiam vitae neque pretium, efficitur neque sed, scelerisque est. Sed convallis diam ut leo interdum sollicitudin. Quisque laoreet interdum ante ut mollis. Fusce vel mauris nec erat gravida pretium. Maecenas porttitor libero tellus, ut tempus quam porta ac. Nulla pretium felis ligula, quis dapibus dolor facilisis in.'
+      },
+      {
+        question: 'Should I derive investment decisions from the data that I see here?',
+        answer: 'Only if you like losing money.'
       }
     ];
     return faqs;
@@ -85,8 +89,8 @@ export class FAQs extends Component {
     // get a list of FAQs
     const faqs = this.getFaqs();
     return (
-      <div>
-        <h2>FAQs:</h2>
+      <div className="FAQs-container">
+        <h1>FAQs:</h1>
           {faqs.length && faqs.map((item, index) => (
             <div key={`item-${index}`} className={`item ${this.state.selectedQuestion === index ? 'open' : ''}`}>
               <p className='question' onClick={() => this.openQuestion(index)}>
