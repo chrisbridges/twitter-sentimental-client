575b79c88d34b98338d4b7c31a7f1cb06a6d0e67
chris.bridges <christopher_bridges@icloud.com>
2018-06-18 16:39:19 -0400

    smoke test for stockSearch passing locally

--- src/components/StockSearch.test.js
+++ src/components/StockSearch.test.js
@@ -1,25 +1,22 @@
-// import React from 'react';
-// import {shallow} from 'enzyme';
-// import StockSearch from './StockSearch';
-// import {Provider} from 'react-redux';
-// import store from '../store';
+import React from 'react';
+import {shallow} from 'enzyme';
+import StockSearch from './StockSearch';
+import {Provider} from 'react-redux';
+import store from '../store';
 
 
-it('returns true', () => {
-  return true;
+it('renders without crashing', () => {
+  shallow(<Provider store={store}><StockSearch /></Provider>);
 });
-// it('renders without crashing', () => {
-//   shallow(<Provider store={store}><StockSearch /></Provider>);
+
+// it('should not submit an empty input', () => {
+
 // });
 
-// // it('should not submit an empty input', () => {
+// it('should update the state with the searched-for stock', () => {
 
-// // });
+// });
 
-// // it('should update the state with the searched-for stock', () => {
+// it('should return to default state upon second search', () => {
 
-// // });
-
-// // it('should return to default state upon second search', () => {
-
-// // });
+// });
